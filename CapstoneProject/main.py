126923357eb147c9ae58d53bad4d9e3b0cfd2406
luckybadge10 <luckybadger10@gmail.com>
2023-01-24 18:54:05 +0000

    progress on services

--- CapstoneProject/main.py
+++ CapstoneProject/main.py
@@ -1,15 +1,28 @@
 from typing import Union
 
 from fastapi import FastAPI
+import { accountService} from ./Services/accountService
 
 app = FastAPI()
+accountService = AcountService()
 
 
-@app.get("/")
-def read_root():
+@app.post("/createAccount/{id}/{address}/{city}/{state}/{zipcode}")
+def create_account(id: int, address, city, state, zipcode):
+    accountService.createAccount(id, address, city, state, zipcode)
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/getAccounts")
+def get_all_account(item_id: int, q: Union[str, None] = None):
+    accountList = accountService.getAllAccounts()
+    return {"account_lists": accountList}
+
+@app.get("/getAccount/{accountId}")
+def get_account(account_id: int, q: Union[str, None] = None):
+    account = accountService.getAccount(account_id)
+    first_name = account.firstName
+    last_name = account.lastName
+    address = account.address
+    email_address = account.emailAddress
+    return {"account_lists": accountList, }

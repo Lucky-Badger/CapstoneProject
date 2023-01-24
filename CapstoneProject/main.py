44df1aa5237d5831768e2a9e69b1b6bde82a63f2
nick-wells <ncew@capgroup.com>
2023-01-24 18:52:36 +0000

    added base models

--- CapstoneProject/main.py
+++ CapstoneProject/main.py
@@ -2,14 +2,20 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from FeatureModules.Services import createAccountService
+
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("createaccount")
 def read_root():
+    createAccountService()
+
+
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+#uvicorn main:app --reload
